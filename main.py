ed06592cf650ca96ed59d0b5f05d2cfcad7101e4
Cássia <cascrisbern@gmail.com>
2022-11-25 15:21:31 -0300

    added comments and readme

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import ujson
 from umqtt.simple import MQTTClient
 
 print("Connecting to WiFi", end="")
+
+# Esse fragmento usa MicroPython network modulo e uma WLAN classe para conectar em uma rede WiFi existente
+# o wokwi usa uma simulação para se conectar ao WiFi e tem a Wokwi-GUEST SSID
+# em uma placa real, precisamos colocar o SSID e a senha para nos conectar em uma rede WiFi real.
+
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 sta_if.connect('Wokwi-GUEST', '')
@@ -15,6 +20,9 @@ print(" Connected!")
 
 print("Connecting to MQTT server... ", end="")
 
+# Nesse fragmento definimos os parametros que serão usados pela classe MQTTClient 
+# para se conectar em um MTQQ broker server existente.
+# Nesse caso, o MQTT broker é um servidor no HiveMQ Cloud.
 
 MQTT_CLIENT_ID = b'abc'
 MQTT_BROKER = b'e3c0fc8531864dbb992e918fc43b028c.s1.eu.hivemq.cloud'
@@ -30,18 +38,25 @@ client = MQTTClient(MQTT_CLIENT_ID,
                     ssl=True,
                     ssl_params = {'server_hostname': b'e3c0fc8531864dbb992e918fc43b028c.s1.eu.hivemq.cloud'}
                     )
-client.connect()
+client.connect() # Aqui é onde acontece a conexão MQTT 
 
 print("Connected!")
 
+# Definimos o IN Pin número 2 e colocamos ela em uma variavél para ler os dados do movimento
 motion_pin = Pin(2, Pin.IN)
+
+# Definimos aqui o OUT Pin número 19 e colocamos ela em uma variável para escrever os dados do movimento
 led_pin = Pin(19, Pin.OUT)
 
-while True:
-    motion_pin_value = motion_pin.value()
+while True: # esse loop infinito
+    motion_pin_value = motion_pin.value() # aqui lemos a variável de movimento
     print('motion_pin_value is')
     print(motion_pin_value)
 
+
+    # se o pino de movimento tem valor 1, significa que o sensor detectou movimento
+    # neste caso, enviamos o valor 1 para o pino do led e o led acenderá
+    # Preparamos também uma mensagem com valor 1 para o MQTT broker
     if motion_pin_value == 1:
         print('Movement detected. Turning the LED ON')
         led_pin.value(1)
@@ -51,6 +66,10 @@ while True:
           'pin': 1
         }
 
+
+    # Se o pino de movimento tiver valor 0, significa que o sensor não detectou movimento
+    # nesse caso, enviaremos valor 0 para o pino do led e o led apagará
+    # Preparamos também uma mensagem com valor 0 para o MQTT broker
     else:
         print('Movement NOT detected. Turning the LED OFF')
         led_pin.value(0)
@@ -62,7 +81,9 @@ while True:
 
     message = ujson.dumps(msg)
 
+    # Enviamos aqui o código para o MQTT broker.
     MQTT_TOPIC = 'cascrisiot01/test01'
     client.publish(MQTT_TOPIC, message)
 
+    # Aqui temos um tempo de espera no nosso loop antes de ler o pino de movimento e executar todas as ações
     time.sleep(0.5)
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Projeto - Objetos Inteligentes
+
+
+## Sobre
+
+Esse é um projeto que faz parte da disciplina Objetos Inteligentes da Universidade Presbiteriana Mackenzie.
+
+Esse projeto visa o desenvolvimento e a implementação de um programa que utilize um sensor, um atuador e o protocolo MQTT.
+
+
+## Detalhes
+
+ Foi criado uma conta no site [Wokwi](https://wokwi.com/) para a criação do programa.
+ Após utilizamos o site [HiveMQ](https://www.hivemq.com/) para conexão MQTT.
+ Após usamos o site [FlowForge](https://flowforge.com/) para o NodeRed.
+ Por fim, usamos os serviços oferecidos pelo [callMeBot](https://www.callmebot.com/blog/free-api-whatsapp-messages/) para mandarmos uma mensagem para um número de whatsApp.
